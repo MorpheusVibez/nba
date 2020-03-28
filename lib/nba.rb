b1c9a358655babc5e271245296e2c64b0a70846e
MorpheusVibez <halodre24@gmail.com>
2020-03-28 17:09:56 -0400

    Moved teams.rb to lib folder/updated nba.rb

--- lib/nba.rb
+++ lib/nba.rb
@@ -1,10 +1,4 @@
 require_relative "./nba/version"
 require "httparty"
-
-# module Nba
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
 require_relative "./nba/cli"
 require_relative "./nba/api"
-require_relative "./nba/teams"
