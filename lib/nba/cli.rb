22168ad914f1d3835340fed39345ba6e97716c02
MorpheusVibez <halodre24@gmail.com>
2020-03-28 16:50:23 -0400

    Updated CLI

--- lib/nba/cli.rb
+++ lib/nba/cli.rb
@@ -1,13 +1,18 @@
 class Nba::CLI
     def call
-        puts "Welcome to the NBA Top 8!"
+        puts "Welcome to the NBA Top 8 Power Rankings!"
         nba_teams
     end
 
     def nba_teams
         puts "1. Milwaukee Bucks"
         puts "2. Los Angeles Lakers"
-        puts "3. Denver Nuggets"
+        puts "3. Los Angeles Clippers"
+        puts "4. Toronto Raptors"
+        puts "5. Boston Celtics"
+        puts "6. Denver Nuggets"
+        puts "7. Utah Jazz"
+        puts "8. Miami Heat"
     end
 
     
