106f177cf186f12a26fb50682261c0c2722567bb
MorpheusVibez <halodre24@gmail.com>
2020-04-01 00:18:16 -0400

    Completed CLI/API relationships.

--- lib/nba/api.rb
+++ lib/nba/api.rb
@@ -1,30 +1,82 @@
-# require "pry"
+require "pry"
 
 class API
-    @@all_teams = []
     def self.teams
         response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
-        # team_name = response["sports"][0]["leagues"][0]["teams"][0]["team"]["displayName"]
         team_name = response["sports"][0]["leagues"][0]["teams"]
-        team_name.each do |hash| 
-            hash["team"]["displayName"]
-                @@all_teams << hash["team"]["displayName"]
-        end
         
-        def self.all_teams
-            @@all_teams
-        end
-
+        @@all_teams = team_name.map {|name| name["team"]["displayName"]}
+    end
+        
+    def self.all_teams
+        @@all_teams
     end
 
-    # def stats
-    #     response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
-    #     # team_name = response["sports"][0]["leagues"][0]["teams"][0]["team"]["displayName"]
-    #     team_name = response["sports"][0]["leagues"][0]["teams"]
-    #     name = []
-    #     team_name.each do |hash|
-    #         name << hash["team"]["record"]["items"][0]["stats"][1].values (returns array)
-    #     end
-    # end
+    def self.team_name(team)
+        response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+        team_name = response["sports"][0]["leagues"][0]["teams"]
 
+        team_name.each do |name|
+            if name["team"]["displayName"] == team
+                @@name = name["team"]["displayName"]
+            end
+        end
+
+        @@name
+    end
+
+    def self.location(team)
+        response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+        team_info = response["sports"][0]["leagues"][0]["teams"]
+        
+        team_info.each do |local|
+            if local["team"]["displayName"] == team
+                @@location = local["team"]["location"]
+            end
+        end
+
+        @@location
+    end
+
+    def self.record(team)
+        response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+        team_info = response["sports"][0]["leagues"][0]["teams"]
+        
+        team_info.each do |rec|
+            if rec["team"]["displayName"] == team
+                @@record = rec["team"]["record"]["items"][0]["summary"]
+            end
+        end
+
+        @@record
+    end
+
+    def self.win_percent(team)
+        response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+        
+        team_info = response["sports"][0]["leagues"][0]["teams"]
+        
+        team_info.each do |per|
+            if per["team"]["displayName"] == team
+                @@win = per["team"]["record"]["items"][0]["stats"][3]["value"]
+            end
+        end
+
+        @@win * 100
+    end
+
+    def self.playoff_seed(team)
+        response = HTTParty.get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+        
+        team_info = response["sports"][0]["leagues"][0]["teams"]
+        
+        team_info.each do |play|
+            if play["team"]["displayName"] == team
+                @@playoff = play["team"]["record"]["items"][0]["stats"][0]["value"]
+            end
+        end
+
+        @@playoff.to_i
+    end
+    
 end
--- lib/nba/cli.rb
+++ lib/nba/cli.rb
@@ -1,7 +1,7 @@
 class CLI
     
     def call
-        puts "Welcome to the NBA Top 8 Power Rankings!"
+        welcome
         list
         menu
     end
@@ -14,28 +14,229 @@ class CLI
                 input = gets.strip.downcase
                 case input
                 when "1"
-                    puts "#{Teams.bucks}"
+                    puts "#{API.team_name(API.all_teams[0])}"
+                    puts "Location: #{API.location(API.all_teams[0])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[0])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[0])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[0])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "2"
-                    puts "#{Teams.lakers}"
+                    puts "#{API.team_name(API.all_teams[1])}"
+                    puts "Location: #{API.location(API.all_teams[1])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[1])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[1])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[1])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "3"
-                    puts "#{Teams.clippers}"
+                    puts "#{API.team_name(API.all_teams[2])}"
+                    puts "Location: #{API.location(API.all_teams[2])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[2])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[2])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[2])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "4"
-                    puts "#{Teams.raptors}"
+                    puts "#{API.team_name(API.all_teams[3])}"
+                    puts "Location: #{API.location(API.all_teams[3])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[3])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[3])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[3])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "5"
-                    puts "#{Teams.celtics}"
+                    puts "#{API.team_name(API.all_teams[4])}"
+                    puts "Location: #{API.location(API.all_teams[4])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[4])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[4])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[4])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "6"
-                    puts "#{Teams.nuggets}"
+                    puts "#{API.team_name(API.all_teams[5])}"
+                    puts "Location: #{API.location(API.all_teams[5])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[5])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[5])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[5])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "7"
-                    puts "#{Teams.jazz}"
+                    puts "#{API.team_name(API.all_teams[6])}"
+                    puts "Location: #{API.location(API.all_teams[6])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[6])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[6])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[6])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "8"
-                    puts "#{Teams.heat}"
+                    puts "#{API.team_name(API.all_teams[7])}"
+                    puts "Location: #{API.location(API.all_teams[7])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[7])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[7])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[7])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "9"
+                    puts "#{API.team_name(API.all_teams[8])}"
+                    puts "Location: #{API.location(API.all_teams[8])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[8])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[8])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[8])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "10"
+                    puts "#{API.team_name(API.all_teams[9])}"
+                    puts "Location: #{API.location(API.all_teams[9])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[9])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[9])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[9])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "11"
+                    puts "#{API.team_name(API.all_teams[10])}"
+                    puts "Location: #{API.location(API.all_teams[10])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[10])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[10])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[10])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "12"
+                    puts "#{API.team_name(API.all_teams[11])}"
+                    puts "Location: #{API.location(API.all_teams[11])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[11])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[11])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[11])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "13"
+                    puts "#{API.team_name(API.all_teams[12])}"
+                    puts "Location: #{API.location(API.all_teams[12])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[12])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[12])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[12])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "14"
+                    puts "#{API.team_name(API.all_teams[13])}"
+                    puts "Location: #{API.location(API.all_teams[13])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[13])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[13])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[13])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "15"
+                    puts "#{API.team_name(API.all_teams[14])}"
+                    puts "Location: #{API.location(API.all_teams[14])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[14])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[14])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[14])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "16"
+                    puts "#{API.team_name(API.all_teams[15])}"
+                    puts "Location: #{API.location(API.all_teams[15])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[15])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[15])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[15])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "17"
+                    puts "#{API.team_name(API.all_teams[16])}"
+                    puts "Location: #{API.location(API.all_teams[16])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[16])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[16])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[16])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "18"
+                    puts "#{API.team_name(API.all_teams[17])}"
+                    puts "Location: #{API.location(API.all_teams[17])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[17])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[17])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[17])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "19"
+                    puts "#{API.team_name(API.all_teams[18])}"
+                    puts "Location: #{API.location(API.all_teams[18])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[18])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[18])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[18])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "20"
+                    puts "#{API.team_name(API.all_teams[19])}"
+                    puts "Location: #{API.location(API.all_teams[19])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[19])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[19])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[19])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "21"
+                    puts "#{API.team_name(API.all_teams[20])}"
+                    puts "Location: #{API.location(API.all_teams[20])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[20])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[20])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[20])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "22"
+                    puts "#{API.team_name(API.all_teams[21])}"
+                    puts "Location: #{API.location(API.all_teams[21])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[21])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[21])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[21])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "23"
+                    puts "#{API.team_name(API.all_teams[22])}"
+                    puts "Location: #{API.location(API.all_teams[22])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[22])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[22])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[22])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "24"
+                    puts "#{API.team_name(API.all_teams[23])}"
+                    puts "Location: #{API.location(API.all_teams[23])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[23])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[23])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[23])}th"
+                    puts "--------------------------"
+                    list
+                    puts "Please select another team or type 'exit' to leave."
+                when "25"
+                    puts "#{API.team_name(API.all_teams[24])}"
+                    puts "Location: #{API.location(API.all_teams[24])}"
+                    puts "Wins-Losses: #{API.record(API.all_teams[24])}"
+                    puts "Win %: #{API.win_percent(API.all_teams[24])}"
+                    puts "Playoff Seed: #{API.playoff_seed(API.all_teams[24])}th"
+                    puts "--------------------------"
+                    list
                     puts "Please select another team or type 'exit' to leave."
                 when "list"
                     list
@@ -43,17 +244,18 @@ class CLI
             end
     end
 
+    def welcome
+        puts "Welcome to the NBA. Please select a team for more information."
+    end
 
     def list
         API.teams
         self.display_team_list
-        #calling api to get list
     end
     
     def display_team_list
-        API.all_teams.each.with_index(1) do |str, index|
-          # binding.pry
-          puts "#{index}. #{str}"
+        API.all_teams.each.with_index(1) do |s, i|
+          puts "#{i}. #{s}"
         end
     end
   
